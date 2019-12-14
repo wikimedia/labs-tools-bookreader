7006f71d4b877282c04ab28d1fbbf2807c5f057a
Jayprakash12345 <0freerunning@gmail.com>
2019-12-14 23:01:32 +0530

    Support for locally uploaded books
    Bug: T240683
    Change-Id: I62bc423090e61c4d2e5c7aaeefa6f5ea94749fe6

--- app.py
+++ app.py
@@ -17,17 +17,26 @@ def read():
     if request.method == 'GET':
         data_v = {}
 
-        # Get File URL
+        # Get Decoded file URL
         wiki_file = request.args.get("file", '')
         wiki_file = urllib.parse.unquote(wiki_file)
 
+        # Check whether ?file is given or not
         if wiki_file == '':
-            return render_template('error.html', msg= 'file parameter is missing' )
+            para_missing = 'file parameter is missing'
+            return render_template('error.html', msg=para_missing)
 
+        # Get file name
         file_array = wiki_file.split('/')
         filename = file_array[-1]
 
-        url = 'https://commons.wikimedia.org/w/api.php'
+        # Get lang and project
+        lang = file_array[2].split('.')[0]
+        project = file_array[2].split('.')[1]
+        data_v["lang"] = lang
+        data_v["project"] = project
+
+        url = 'https://{0}.{1}.org/w/api.php'.format(lang, project)
 
         params = {
             "action": "query",
@@ -42,13 +51,14 @@ def read():
         data = r.json()
 
         try:
-            data_v["book_url"] = data["query"]["pages"][0]["imageinfo"][0]["url"]
-            data_v["numofpage"] = data["query"]["pages"][0]["imageinfo"][0]["pagecount"]
+            page = data["query"]["pages"][0]
+            data_v["book_url"] = page["imageinfo"][0]["url"]
+            data_v["numofpage"] = page["imageinfo"][0]["pagecount"]
 
             return render_template('read.html', data=data_v)
 
-        except:
-            return render_template('error.html', msg='')
+        except KeyError as ex:
+            return render_template('error.html', msg=ex)
 
     else:
         abort(400)
