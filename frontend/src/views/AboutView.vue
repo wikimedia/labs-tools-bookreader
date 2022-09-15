8f7b6d45e94c94dce6a47704d0c452eca3871519
Jayprakash12345 <0freerunning@gmail.com>
2022-09-15 21:40:40 +0530

    Add source code link
    Change-Id: I7cb6bbfcc4ffaf1e0ab883ba8069bf13f0dc9058

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -9,10 +9,17 @@
           {{ $t('context-text') }}: <a target="__blank" href="https://meta.wikimedia.org/wiki/Community_Wishlist_Survey_2019/Multimedia_and_Commons/Improve_the_PDF/book_reader">Improve the PDF/book reader</a> {{ $t('and-text') }}
           <a target="__blank" href="https://meta.wikimedia.org/wiki/Community_Wishlist_Survey_2016/Categories/Commons#Implement_Internet_Archive_BookReader_in_Commons_&_Wikisource">Implement Internet Archive BookReader in Commons & Wikisource</a>.
         </p>
+        <br/>
         <h3>{{ $t('createby-heading') }}</h3>
         <p>
           <a target="__blank" href="https://meta.wikimedia.org/wiki/User:Jayprakash12345">Jayprakash</a>
         </p>
+        <br/>
+        <h3>{{ $t('source-code') }}</h3>
+        <p>
+          <a target="__blank" href="https://gerrit.wikimedia.org/g/labs/tools/bookreader">https://gerrit.wikimedia.org/g/labs/tools/bookreader</a>
+        </p>
+        <br/>
         <h3>{{ $t('poweredby-heading') }}</h3>
         <p>
           <a target="__blank" href="https://github.com/internetarchive/bookreader">Internet Archive BookReader</a>
