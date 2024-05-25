e9588a2ecf085a71df490998f12b3b6bfb7268b9
Kalli navya <navyasrikalli1999@gmail.com>
2024-05-25 13:33:31 +0000

    Show Languages name instead of Language code
    Bug: T365914
    Change-Id: I9116ca6118afbc93b25466e1f81b5568bdf31060

--- frontend/src/views/ChangeLanguageView.vue
+++ frontend/src/views/ChangeLanguageView.vue
@@ -4,10 +4,14 @@
       <v-sheet min-height="80vh" rounded="lg" style="padding: 12px">
         <h1 class="text-center">{{ $t('my-language') }}</h1>
         <v-row>
-          <v-col vcols="12" md="4">
+          <v-col cols="12" md="4">
             <v-select
-              :items="langs" :label="$t('selected-language')"
-              outlined v-model="$i18n.locale"
+              :items="langs"
+              :label="$t('selected-language')"
+              outlined
+              v-model="$i18n.locale"
+              item-text="name"
+              item-value="code"
             ></v-select>
             <v-btn elevation="2" color="primary" outlined @click="savePreference">
               {{ $t('button-save') }}
@@ -24,16 +28,23 @@ import { availableLanguage } from '@/i18n';
 
 export default {
   name: 'ChangeLanguageView',
-  data: function(){
+  data() {
     return {
-      "langs": availableLanguage
-    }
+      langs: this.getLanguagesWithNames(availableLanguage)
+    };
   },
   methods: {
-    savePreference: function(){
-      localStorage.setItem('ws-bookread-lang', this.$i18n.locale)
-      this.$alert( this.$t('preference-saved'), this.$t('success-text'), "success")
+    getLanguagesWithNames(codes) {
+      const displayNames = new Intl.DisplayNames(['en'], { type: 'language' });
+      return codes.map(code => ({
+        code,
+        name: displayNames.of(code)
+      }));
+    },
+    savePreference() {
+      localStorage.setItem('ws-bookread-lang', this.$i18n.locale);
+      this.$alert(this.$t('preference-saved'), this.$t('success-text'), 'success');
     }
   }
-}
+};
 </script>
